769478c9de438f91691e991852cb24306e4f477e
chrdi <40054304+chrdi@users.noreply.github.com>
2019-03-12 06:31:08 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
-* Obscure login error */
+/******** Obscure login error ********/
 function custom_wordpress_error_message(){
 return 'Try again.';
 }
 add_filter( 'login_errors', 'custom_wordpress_error_message' );
 
-// reduce revisions //
+/******** reduce revisions ********/
 add_filter( 'wp_revisions_to_keep', 'divi_limit_revisions', 10, 2 );
 function divi_limit_revisions( $num ) { 
 $num = 3;
